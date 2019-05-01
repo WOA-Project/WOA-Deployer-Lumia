dea9f321af5be39094fa045f8609b186a4ccf5f4
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-05-01 15:35:08 +0200

    Ability to differentiate Dual and Single SIM

--- Source/Deployer.Lumia/WoaDeployer.cs
+++ Source/Deployer.Lumia/WoaDeployer.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Deployer.Execution;
+using Deployer.Services.Wim;
 using Serilog;
 
 namespace Deployer.Lumia
@@ -10,25 +12,31 @@ namespace Deployer.Lumia
     {
         private readonly ITooling tooling;
         private readonly IPhone phone;
+        private readonly IWindowsOptionsProvider optionsProvider;
+        private readonly IFileSystemOperations fileSystemOperations;
         private readonly IScriptRunner scriptRunner;
         private readonly IScriptParser parser;
 
-        public WoaDeployer(IScriptRunner scriptRunner, IScriptParser parser, ITooling tooling, IPhone phone)
+        public WoaDeployer(IScriptRunner scriptRunner, IScriptParser parser, ITooling tooling, IPhone phone,
+            IWindowsOptionsProvider optionsProvider,
+            IFileSystemOperations fileSystemOperations)
         {
             this.scriptRunner = scriptRunner;
             this.parser = parser;
             this.tooling = tooling;
             this.phone = phone;
+            this.optionsProvider = optionsProvider;
+            this.fileSystemOperations = fileSystemOperations;
         }
 
         public async Task Deploy()
         {
             var dict = new Dictionary<(PhoneModel, Variant), string>
             {
-                {(PhoneModel.Talkman, Variant.SingleSim), Path.Combine("Scripts", "Talkman", "Single-Sim.txt")},
-                {(PhoneModel.Cityman, Variant.SingleSim), Path.Combine("Scripts", "Cityman", "Single-Sim.txt")},
-                {(PhoneModel.Talkman, Variant.DualSim), Path.Combine("Scripts", "Talkman", "Double-Sim.txt")},
-                {(PhoneModel.Cityman, Variant.DualSim), Path.Combine("Scripts", "Cityman", "Double-Sim.txt")},
+                {(PhoneModel.Talkman, Variant.SingleSim), Path.Combine("Scripts", "Talkman", "SingleSim.txt")},
+                {(PhoneModel.Cityman, Variant.SingleSim), Path.Combine("Scripts", "Cityman", "SingleSim.txt")},
+                {(PhoneModel.Talkman, Variant.DualSim), Path.Combine("Scripts", "Talkman", "DualSim.txt")},
+                {(PhoneModel.Cityman, Variant.DualSim), Path.Combine("Scripts", "Cityman", "DualSim.txt")},
             };
 
             var phoneModel = await phone.GetModel();
@@ -36,7 +44,31 @@ namespace Deployer.Lumia
             var path = dict[(phoneModel.Model, phoneModel.Variant)];
 
             await scriptRunner.Run(parser.Parse(File.ReadAllText(path)));
+            await PatchBootManagerIfNeeded();
             await PreparePhoneDiskForSafeRemoval();
+        }
+
+        private async Task PatchBootManagerIfNeeded()
+        {
+            Log.Debug("Checking if we have to patch WOA's Boot Manager");
+            var options = optionsProvider.Options;
+            using (var file = File.OpenRead(options.ImagePath))
+            {
+                var imageReader = new WindowsImageMetadataReader();
+                var windowsImageInfo = imageReader.Load(file);
+
+                var selectedImage = options.ImageIndex - 1;
+                if (int.TryParse(windowsImageInfo.Images[selectedImage].Build, out var buildNumber))
+                {
+                    if (buildNumber == 17763)
+                    {
+                        Log.Verbose("Build 17763 detected. Patching Boot Manager.");
+                        var dest = Path.Combine((await phone.GetSystemVolume()).Root, "EFI", "Boot") + Path.PathSeparator;
+                        await fileSystemOperations.Copy(@"Core\Boot\bootaa64.efi", dest);
+                        Log.Verbose("Boot Manager Patched.");
+                    }
+                }
+            }
         }
 
         private async Task PreparePhoneDiskForSafeRemoval()
